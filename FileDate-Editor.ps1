4dfbd642a9390b3ac940ad0c91d5502203506b3a
Charlotte.Hong <hunandy14@gmail.com>
2022-02-13 16:12:14 +0900

    完成JP的簡化版

--- FileDate-Editor.ps1
+++ FileDate-Editor.ps1
@@ -70,14 +70,14 @@ function FileDateEditor {
         [DateTime] $AllDate,
         [Parameter(ParameterSetName = "Time")]
         [DateTime] $CreationTime,
-        [Parameter(ParameterSetName = "Time")]
-        [DateTime] $LastWriteTime,
         [Parameter(Position = 1, ParameterSetName = "Time")]
         [DateTime] $LastAccessTime,
+        [Parameter(ParameterSetName = "Time")]
+        [DateTime] $LastWriteTime,
         [Parameter(ParameterSetName = "")]
         [switch] $Preview
     )
-
+    
     if (($null -eq $LastAccessTime) -and
         ($null -eq $LastWriteTime) -and
         ($null -eq $CreationTime) -and
@@ -95,36 +95,64 @@ function FileDateEditor {
         if (!($Files -is [array])) { $Files = @($Files) }
         for ($i = 0; $i -lt $Files.Count; $i++) {
             $File = $Files[$i]
-            if ($Preview) {
-                Write-Host "[$($i+1)]" $File.FullName -ForegroundColor:Yellow
-                Write-Host "  " $File.CreationTime   "--> " -NoNewline
-                Write-Host $CreationTime -ForegroundColor:DarkBlue
-                Write-Host "  " $File.LastWriteTime  "--> " -NoNewline
-                Write-Host $LastWriteTime -ForegroundColor:DarkBlue
-                Write-Host "  " $File.LastAccessTime "--> " -NoNewline
-                Write-Host $LastAccessTime -ForegroundColor:DarkBlue
-            } else {
+            Write-Host "[$($i+1)]" $File.FullName -ForegroundColor:Yellow
+            Write-Host "  " $File.CreationTime   "--> " -NoNewline
+            Write-Host $CreationTime -ForegroundColor:DarkBlue
+            Write-Host "  " $File.LastWriteTime  "--> " -NoNewline
+            Write-Host $LastWriteTime -ForegroundColor:DarkBlue
+            Write-Host "  " $File.LastAccessTime "--> " -NoNewline
+            Write-Host $LastAccessTime -ForegroundColor:DarkBlue
+            # if (!$Preview) {
                 if ($CreationTime  ) { $File.CreationTime   = $CreationTime   }
                 if ($LastWriteTime ) { $File.LastWriteTime  = $LastWriteTime  }
                 if ($LastAccessTime) { $File.LastAccessTime = $LastAccessTime }
-            }
+            # }
         }
     }
 }
-# FileDateEditor (Get-Item "README.md") "2022/02/13 午前 04:00:02"
-# $Date = New-DateTime "2022/02/13 午前 04:55:55" -JP
+# $Date = New-DateTime "2022/02/13 午前 00:00:00" -JP
 # $File = Get-Item ".\README.md"
 # $File = Get-ChildItem "Test" -Recurse
+# FileDateEditor -File:$File -AllDate:$Date
 
 # FileDateEditor -File:$File -AllDate:$Date -Preview
 # FileDateEditor -File:$File -LastAccessTime:$Date -Preview
 # FileDateEditor -File:$File $Date -Preview
-# FileDateEditor $File -Preview
+# FileDateEditor -File:$File -Preview
 
-# FileDateEditor $File -CreationTime:$Date -Preview
-# FileDateEditor $File -LastWriteTime:$Date -Preview
-# FileDateEditor $File -LastAccessTime:$Date -Preview
-# FileDateEditor $File
+# FileDateEditor -File:$File -CreationTime:$Date -Preview
+# FileDateEditor -File:$File -LastAccessTime:$Date -Preview
+# FileDateEditor -File:$File -LastAccessTime:$Date -Preview
+# FileDateEditor -File:$File
 
-# FileDateEditor $File -LastAccessTime:$Date -Preview
+# FileDateEditor -File:$File LastAccessTime:$Date -Preview
 # FileDateEditor
+# return
+
+function ChangeWriteTime_JP {
+    [CmdletBinding(DefaultParameterSetName = "Time")]
+    param (
+        [Parameter(Position = 0, ParameterSetName = "", Mandatory=$true)]
+        [string] $Path,
+        [Parameter(Position = 1, ParameterSetName = "Time")]
+        [string] $Date,
+        [Parameter(ParameterSetName = "")]
+        [switch] $Preview
+    )
+    if (Test-Path $Path -PathType:Leaf) {
+        $Files = @(Get-Item $Path)
+    } elseif (Test-Path $Path -PathType:Container) {
+        $Files = Get-ChildItem $Path -Recurse
+    }
+    
+    if ($Date -eq "") {
+        FileDateEditor $Files
+    } else {
+        $Date2  = New-DateTime $Date -JP
+        FileDateEditor $Files -LastWriteTime:$Date2 -LastAccessTime:$Date2 -Preview:$Preview
+    }
+}
+
+# ChangeWriteTime_JP -Path:"README.md"
+# ChangeWriteTime_JP -Path:"Test"
+ChangeWriteTime_JP -Path:"Test" -Date:"2022/02/13 午前 00:00:00"
